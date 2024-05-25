072514262442342b75b003c58bf14a1979b96f05
Dariia Lavinska <d.lavinska@gmail.com>
2024-05-25 12:08:26 +0300

    add hw-5

---
+++ vue-home-work-5/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <CardBox/>
+</template>
+
+<script>
+import CardBox from './components/CardBox.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CardBox,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 120px;
+}
+</style>
