a25937d0513626c88079ce596c5860fb145b1423
Dariia Lavinska <d.lavinska@gmail.com>
2024-05-25 10:13:44 +0300

    add hw-2

---
+++ index.js
@@ -0,0 +1,18 @@
+const findSummary = (a, b) => {
+  const sum = a + b;
+  return sum;
+};
+
+const findSubstract = (a, b) => {
+  const sub = a - b;
+  return sub;
+};
+
+const findMultiply = (a, b) => {
+  const mult = a * b;
+  return mult;
+};
+
+findSummary(2, 4);
+findSubstract(6, 2);
+findMultiply(5, 4);
