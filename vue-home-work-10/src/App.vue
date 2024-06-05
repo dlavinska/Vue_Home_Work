9aa510d3fbaa70118ac1e130d0b52c039e3844c0
Dariia Lavinska <d.lavinska@gmail.com>
2024-06-05 19:38:11 +0300

    first commit hw10

---
+++ vue-home-work-10/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <AlertComponent :type="'warning'" :text="'What is love?'" />
+</template>
+
+<script>
+import AlertComponent from './components/AlertComponent'
+
+export default {
+  name: 'App',
+  components: {
+    AlertComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-home-work-10/src/components/AlertComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div :class="`alert alert-${type}`" role="alert">{{text}}</div>
+</template>
+
+<script>
+export default {
+  name: 'AlertComponent',
+  props: {
+    text: {
+      type: String,
+      default: 'Some text',
+      required: true
+    },
+    type: {
+      type: String,
+      default: 'success',
+      required: true,
+      computed: (value) => {
+        return ['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'light', 'dark'].includes(value)
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
