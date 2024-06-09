27c1f46da5b407d34786c166b542601237fffa77
Dariia Lavinska <d.lavinska@gmail.com>
2024-06-09 22:18:30 +0300

    add hw-9

---
+++ vue-home-work/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <ProgressComponent :percentage='40'/>
+</template>
+
+<script>
+import ProgressComponent from './components/ProgressComponent'
+
+export default {
+  name: 'App',
+  components: {
+    ProgressComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-home-work/src/components/ProgressComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="progress">
+    <div class="progress-bar" 
+      role="progressbar" 
+      :aria-valuenow="percentage" 
+      aria-valuemin="0" 
+      aria-valuemax="100" 
+      aria-label="progressbar" 
+      :style="{ width: percentage + '%' }"
+    >
+      {{percentage + `%`}}
+    </div>
+  
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProgressComponent',
+  props: {
+    percentage: {
+      type: Number,
+      default: 0,
+      required: true,
+      validator: (value) => {
+        return value >=0 && value <=100;
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
