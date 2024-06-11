9c1bc0624d25f52ba444deb109d9980c20a6f894
Dariia Lavinska <d.lavinska@gmail.com>
2024-06-11 22:17:31 +0300

    add hw-13

---
+++ vue-home-work-13/src/components/BtnGroup.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="btn-group" role="group">
+    <button type="button" class="btn btn-secondary left" :class="{active: activeClass === 'left'}" @click="setActiveClass('left')">Left</button>
+    <button type="button" class="btn btn-secondary right" :class="{active: activeClass === 'right'}" @click="setActiveClass('right')">Right</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BtnGroup',
+  data() {
+    return {
+      activeClass:'',
+    }
+  },
+  methods: {
+    setActiveClass(classBtnName) {
+      this.activeClass = classBtnName;
+    }
+
+  }
+}
+</script>
