7323859c7086c47b38432ea6c31fe45466644ed5
Dariia Lavinska <d.lavinska@gmail.com>
2024-06-15 23:01:06 +0300

    add home-work-16

---
+++ vue-home-work-16/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Приклад компонента Collapse</h1>
+    <CollapseComponent text="Це прихований текст" :opened="true"/>
+  </div>
+</template>
+
+<script>
+import CollapseComponent from './components/CollapseComponent'
+
+export default {
+  name: 'App',
+  components: {
+    CollapseComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-home-work-16/src/components/CollapseComponent.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <p>
+      <a class="btn btn-primary"
+        data-bs-toggle="collapse"
+        href="#"
+        role="button"
+        aria-expanded="false"
+        @click.prevent="toggleContent"
+      >
+        Link with href
+      </a>
+    </p>
+    <div class="collapse" :class="{show: isCollapsing}">
+      <div class="card card-body">
+        {{text}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CollapseComponent',
+  props: {
+    text: {
+      type: String,
+      required: true,
+      default: ''
+    },
+    opened: {
+      type: Boolean,
+      required: true,
+      default: true
+    }
+  },
+  data() {
+    return {
+      isCollapsing: this.opened,
+    }
+  },
+  methods: {
+    toggleContent() {
+      this.isCollapsing = !this.isCollapsing;
+    }
+  }
+}
+</script>
