d7a8dbe77393ec0dec67f98e7ac57bab1e8feb90
Dariia Lavinska <d.lavinska@gmail.com>
2024-06-29 10:06:56 +0300

    add home-work-12

---
+++ vue-home-work-12/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <ListGroup>
+    <p>one</p>
+    <p>two</p>
+  </ListGroup>
+</template>
+
+<script>
+import ListGroup from './components/ListGroup'
+
+export default {
+  name: 'App',
+  components: {
+    ListGroup
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-home-work-12/src/components/ListGroup.vue
@@ -0,0 +1,14 @@
+<template>
+  <ul class="list-group">
+    <li class="list-group-item" v-for="(children, index) in $slots.default()" :key="index">
+      <component :is="children"/>
+    </li>
+
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'ListGroup',
+}
+</script>
