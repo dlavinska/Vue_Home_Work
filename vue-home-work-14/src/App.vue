ab7710996182d042e45e083ad092f7f35cd8263c
Dariia Lavinska <d.lavinska@gmail.com>
2024-07-07 10:00:36 +0300

    add hw-14

---
+++ vue-home-work-14/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <CarouselComponent :images="images"/>
+</template>
+
+<script>
+import CarouselComponent from './components/CarouselComponent'
+
+export default {
+  name: 'App',
+  components: {
+    CarouselComponent
+  },
+  data() {
+    return {
+      images: [{title: 'sunflowers', src: './images/first.jpg'}, {title:'cat', src: './images/second.jpg'}, {title: 'butterfly', src: './images/third.jpg'}]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-home-work-14/src/components/CarouselComponent.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="carousel" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      <div v-for="(elem, index) in images" :key="index" class="carousel-item" :class="{active: index === activeIndex}">
+        <img :alt="elem.title" class="d-block w-100" :src="elem.src">
+      </div>
+    </div>
+    <button class="carousel-control-prev" data-bs-target="#carousel" type="button" data-bs-slide="prev" @click="prevPicture">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" data-bs-target="#carousel" type="button" data-bs-slide="next" @click="nextPicture">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CarouselComponent',
+  props: {
+    images: {
+      type: Array,
+      required: true,
+      default: () => [],
+    }
+  },
+  data() {
+    return {
+      activeIndex: 0,
+    }
+  },
+  methods: {
+    prevPicture() {
+      this.activeIndex = (this.activeIndex - 1 + this.images.length) % this.images.length
+    },
+    nextPicture() {
+      this.activeIndex = (this.activeIndex + 1) % this.images.length
+    }
+  }
+}
+</script>
